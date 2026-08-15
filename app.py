362b66e89aec236c6aadb9e6e3e45718be51b05f
faizan2656 <faizantahir006@gmail.com>
2026-08-15 04:21:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -167,9 +167,5 @@ def submit_assessment():
 
   return redirect(target_url)
 
-
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000, debug=True)
-
-# Ye line Vercel ke liye lazmi add karein:
-app = app
+if __name__ == '__main__':
+    app.run()
